485fb266e0d329901695e4aa213194fe9cec3824
Eddie Webb <ollitech@gmail.com>
2022-08-30 14:04:16 -0400

    tolerate boto errors

--- src/frontend/frontend.py
+++ src/frontend/frontend.py
@@ -630,7 +630,7 @@ def create_app():
             if tags["Key"] == 'aws:eks:cluster-name':
                 cluster_name = tags["Value"]
                 break
-    except (RequestException, HTTPError) as err:
+    except (Exception) as err:
         app.logger.warning(
             "Unable to retrieve cluster name from Deployment manifest.")
 
