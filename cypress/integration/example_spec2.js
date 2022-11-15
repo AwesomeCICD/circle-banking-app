f04eabbe0fbdf90ce71d6a6b42a6e06f17cbf221
Brian O'Halloran <brian_o_halloran@hotmail.com>
2022-11-15 17:54:38 +0000

    Amending second test to remove duplicate lines and commented out ones

--- cypress/integration/example_spec2.js
+++ cypress/integration/example_spec2.js
@@ -1,8 +1,6 @@
 describe('Simple tests', () => {
   it('Visit Test website', () => {
     cy.visit('https://dev.cera.circleci-labs.com/');
-#    cy.get('.btn').click() // Click on button
-#    cy.focused().click() // Click on el with focus
     cy.contains('Sign In').click() // Click on first el containing 'Sign In'
   });
 });
