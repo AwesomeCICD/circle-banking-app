1f35fbfb6a1d2305189723cb3487bd41c2ac0898
Brian O'Halloran <brian_o_halloran@hotmail.com>
2022-11-16 17:24:22 +0000

    Tryng using submit form type

--- cypress/integration/example_spec2.js
+++ cypress/integration/example_spec2.js
@@ -1,6 +1,6 @@
 describe('Simple tests', () => {
   it.only('click command', () => {
     cy.visit('https://dev.cera.circleci-labs.com/login');
-    cy.get('.card-body sign-in').contains('Sign In').click()
+    cy.get('button[type="submit"]').click();
   });
 });
