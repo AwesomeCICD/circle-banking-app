234db0667743ddb1d749aac6aa07ee1e408ebbec
Brian O'Halloran <brian_o_halloran@hotmail.com>
2022-11-16 18:09:55 +0000

    Tryng using .mb-4 array element but incrementing to 1 from zero

--- cypress/integration/example_spec2.js
+++ cypress/integration/example_spec2.js
@@ -1,6 +1,6 @@
 describe('Simple tests', () => {
   it.only('click command', () => {
     cy.visit('https://dev.cera.circleci-labs.com/login');
-    cy.get('.mb-4').eq(0).click();
+    cy.get('.mb-4').eq(1).click();
   });
 });
