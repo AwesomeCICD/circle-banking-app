e1ecd0738ab7e44e54d6a408f92783fd69fe1009
Brian O'Halloran <brian_o_halloran@hotmail.com>
2022-11-16 16:38:38 +0000

    Trying other button instead and taking out excessive brackets

--- cypress/integration/example_spec2.js
+++ cypress/integration/example_spec2.js
@@ -1,8 +1,4 @@
 describe('Simple tests', () => {
-    before(() => {
-        // cy.visit('/')
-    }}
-
   it.only('click command', () => {
     cy.visit('https://dev.cera.circleci-labs.com/login');
     cy.get('input[id=create-account-btn]').click() // Click on 'Create an Account' button
