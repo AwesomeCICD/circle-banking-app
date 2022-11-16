e469432131e2fc854716bdb3bb175d3edae0e619
Brian O'Halloran <brian_o_halloran@hotmail.com>
2022-11-16 16:11:02 +0000

    Chan ging command to be simple instead with .btn-sign-in format

--- cypress/integration/example_spec2.js
+++ cypress/integration/example_spec2.js
@@ -1,6 +1,6 @@
 describe('Simple tests', () => {
   it('Visit Test website', () => {
     cy.visit('https://dev.cera.circleci-labs.com/');
-    cy.get('.btn'). click() // Click on 'Sign In' button
+    cy.get('.btn-sign-in'). click() // Click on 'Sign In' button
   });
 });
