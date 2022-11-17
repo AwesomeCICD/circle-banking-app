9fccabc189374f49adaef812479d45512b8e9e99
Brian O'Halloran <brian_o_halloran@hotmail.com>
2022-11-17 12:01:14 +0000

    Using wildcard with Class Name to find button

--- cypress/integration/example_spec.js
+++ cypress/integration/example_spec.js
@@ -1,6 +1,9 @@
 describe('Simple tests', () => {
   it.only('click command', () => {
     cy.visit('https://dev.cera.circleci-labs.com/login');
-    cy.get('form').submit()
+    cy.get('button[class*="btn-sign-in"]').should(verifyText =>
+    {
+     expect(verifyText).have.text('Sign In')
+    }.click()
   });
 });
