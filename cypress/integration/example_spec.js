1f430d4d2938b132912c8856b7e5e0f6ff771f72
Brian O'Halloran <brian_o_halloran@hotmail.com>
2022-11-17 12:20:20 +0000

    Using Selector path to do click

--- cypress/integration/example_spec.js
+++ cypress/integration/example_spec.js
@@ -1,6 +1,6 @@
 describe('Simple tests', () => {
   it.only('click command', () => {
     cy.visit('https://dev.cera.circleci-labs.com/login');
-    cy.get('.btn.btn-sign-in.btn-block.mb-4').click()
+    cy.get('#login-form > button').click()
   });
 });
