cff7275a3e68c65f0be5434559af7af824861240
Brian O'Halloran <brian_o_halloran@hotmail.com>
2022-11-17 12:05:08 +0000

    Using wildcard with Class Name to find button but simplifies to remove text search

--- cypress/integration/example_spec.js
+++ cypress/integration/example_spec.js
@@ -1,9 +1,6 @@
 describe('Simple tests', () => {
   it.only('click command', () => {
     cy.visit('https://dev.cera.circleci-labs.com/login');
-    cy.get('button[class*="btn-sign-in"]').should(verifyText =>
-    {
-     expect(verifyText).have.text('Sign In')
-    }.click()
+    cy.get('button[class*="btn-sign-in"]').click()
   });
 });
