45b31e7f5fdbe3885c92ed7337d43928c50f38e2
Ryan Pedersen <ryanpedersen42@gmail.com>
2023-01-04 17:05:00 -0500

    add in triggers

---
+++ triggers.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# kick off a few commits 
+
+kickOffFail () {
+    git checkout rp/failed-test
+    git commit --allow-empty -m "test commit"
+    git push origin rp/failed-test
+    git checkout rp/cool-feature
+}
+
+kickOffPass () {
+    git checkout rp/cool-feature
+    git add .
+    git commit -m "fixed policy"
+    git push origin rp/cool-feature
+}
+
+kickOffPass
+kickOffFail
