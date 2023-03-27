291854a30fa65735404a6c0b2998dbe90e28047e
Eddie Webb <ollitech@gmail.com>
2023-03-27 14:16:01 -0400

    fix pyhon linting errors

--- src/frontend/frontend.py
+++ src/frontend/frontend.py
@@ -37,7 +37,6 @@ import logging
 import os
 import socket
 from decimal import Decimal, DecimalException
-from time import sleep
 import boto3
 
 import requests
@@ -349,7 +348,6 @@ def create_app():
             resp.raise_for_status()  # Raise on HTTP Status code 4XX or 5XX
         except requests.exceptions.HTTPError as http_request_err:
             raise UserWarning(resp.text) from http_request_err
- 
 
     def _add_contact(label, acct_num, routing_num, is_external_acct=False):
         """
