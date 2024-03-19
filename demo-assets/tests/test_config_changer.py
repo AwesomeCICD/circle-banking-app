d8af53f9879368da54b2be7ef1a61c70d416c9be
Eddie Webb <ollitech@gmail.com>
2024-03-19 10:26:05 -0400

    fix job lookup for new multi region config

--- demo-assets/tests/test_config_changer.py
+++ demo-assets/tests/test_config_changer.py
@@ -15,25 +15,25 @@ def test_config_is_loaded(default_config_changer):
 
 def test_can_get_named_jobs_simple_entry(default_config_changer):
     name = 'python-test'
-    job = default_config_changer.get_workflow_job_named(name)
+    job = default_config_changer.get_workflow_job_with_prefix(name)
     assert(job['name'] == name )
 
 
 def test_can_get_named_jobs_entry_with_info(default_config_changer):
     name = 'java-checkstyle'
-    job = default_config_changer.get_workflow_job_named(name)
+    job = default_config_changer.get_workflow_job_with_prefix(name)
     assert(job['name'] == name )
 
 def test_can_get_named_jobs_entry_with_info_and_name(default_config_changer):
     name = 'Deploy Dev'
-    job = default_config_changer.get_workflow_job_named(name)
-    assert(job['name'] == name )
+    job = default_config_changer.get_workflow_job_with_prefix(name)
+    assert(job['name'].startswith(name) )
 
 
 def test_can_modify_context(default_config_changer):
     job = default_config_changer.the_dev_deploy_workflow_definition()
     job['context'].append("boop")
-    assert(job['name'] == "Deploy Dev" )
-    newjob = default_config_changer.get_workflow_job_named("Deploy Dev")
+    assert(job['name'].startswith('Deploy Dev')  )
+    newjob = default_config_changer.get_workflow_job_with_prefix("Deploy Dev")
     assert("boop" in newjob['context'] )
    
