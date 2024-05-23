c800e2f470d7050b78e352fe9efd5cfdbb693244
Eddie Webb <ollitech@gmail.com>
2024-05-23 12:21:40 -0400

    try it

--- .circleci/ortelius.bash
+++ .circleci/ortelius.bash
@@ -5,5 +5,7 @@ export DOCKERREPO=${SKAFFOLD_IMAGE_REPO}
 export IMAGE_TAG=${SKAFFOLD_IMAGE_TAG}  #skafoold sets from app_version
           
 
+pip install ortelius-cli
+python -m pip install cyclonedx-bom
 cyclonedx-py --requirements
 dh updatecomp --rsp ortelius.toml --deppkg "cyclonedx@cyclonedx.json"
