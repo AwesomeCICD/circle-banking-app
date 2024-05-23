1cea48c57f2ad4bc6f71c3384fa87b50aef76b42
Eddie Webb <ollitech@gmail.com>
2024-05-23 13:46:38 -0400

    try exporting image_tag

--- .circleci/ortelius.bash
+++ .circleci/ortelius.bash
@@ -2,7 +2,8 @@ export DHURL="https://console.deployhub.com"
 export DHUSER=eddiewebb-ci
 export DHPASS=logincci
 export DOCKERREPO=${SKAFFOLD_IMAGE_REPO}
-export IMAGE_TAG=${SKAFFOLD_IMAGE_TAG}  #skafoold sets from app_version
+export IMAGE_TAG=${SKAFFOLD_IMAGE_TAG}  #skafoold sets from git tree specific sha
+echo "export IMAGE_TAG=${SKAFFOLD_IMAGE_TAG}" >> $BASH_ENV #make available to parent CCI job steps that come
           
 
 pip install ortelius-cli
