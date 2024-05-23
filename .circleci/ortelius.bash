b723f810ca3c7e5a34f34c5362a1a16e3f1146cc
Eddie Webb <ollitech@gmail.com>
2024-05-23 16:57:33 -0400

    fix manifest

--- .circleci/ortelius.bash
+++ .circleci/ortelius.bash
@@ -1,13 +1,20 @@
+echo "++++++++++"
+echo "REPO: ${SKAFFOLD_IMAGE_REPO}"
+echo "IMAGE: ${SKAFFOLD_IMAGE}"
+echo "TAG: ${SKAFFOLD_IMAGE_TAG}"
+echo "NAME: ${SKAFFOLD_IMAGE%":$SKAFFOLD_IMAGE_TAG"}"
+echo "+++++++++++"
+
+
 export DHURL="https://console.deployhub.com"
 export DHUSER=eddiewebb-ci
 export DHPASS=logincci
-export DOCKERREPO=${SKAFFOLD_IMAGE_REPO}
+export DOCKERREPO=${SKAFFOLD_IMAGE%":$SKAFFOLD_IMAGE_TAG"}
 export IMAGE_TAG=${SKAFFOLD_IMAGE_TAG}  #skafoold sets from git tree specific sha
-echo "export IMAGE_TAG=${SKAFFOLD_IMAGE_TAG}" >> $BASH_ENV #make available to parent CCI job steps that come
+#echo "export IMAGE_TAG=${SKAFFOLD_IMAGE_TAG}" >> $BASH_ENV #make available to parent CCI job steps that come
           
 pip install ortelius-cli
 python3 -m pip install cyclonedx-bom
-pushd $SKAFFOLD_BUILD_CONTEXT
+cd $SKAFFOLD_BUILD_CONTEXT
 cyclonedx-py requirements > cyclonedx@cyclonedx.json
 dh updatecomp --rsp ortelius.toml --deppkg "cyclonedx@cyclonedx.json"
-popd
