64ba7c2cc287445be887f42ef2482c060764d258
Eddie Webb <ollitech@gmail.com>
2025-03-11 16:10:27 -0400

    reset registry on each test

--- src/userservice/extensions.py
+++ src/userservice/extensions.py
@@ -1,14 +1,16 @@
 """
 Extensions for app
 """
+import os
 from prometheus_flask_exporter.multiprocess import GunicornPrometheusMetrics
 #from prometheus_flask_exporter import PrometheusMetrics
-
+import tempfile
 metrics = GunicornPrometheusMetrics.for_app_factory()
 
 def setup_extensions(app):
     """
     Allows us to implement metrics and reset from testsuite.
     """
+    os.environ["PROMETHEUS_MULTIPROC_DIR"] = tempfile.gettempdir()
     metrics.init_app(app)
     return app
